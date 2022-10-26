3cce972ffacac3691c0eaad18507ec0044cd7cd7
AlbinHall <albin-hall1@hotmail.com>
2022-10-26 19:07:06 +0000

    initial commit

--- run.py
+++ run.py
@@ -1,3 +1,24 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+import time
+
+red = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35]
+black = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36]
+green = [0]
+
+
+def Choice_of_color():
+
+
+def random_number_selector():
+
+
+def the_bank():
+
+
+def calculate_the_stake():
+
+
+def losing_the_bet
+
+
+def main()
