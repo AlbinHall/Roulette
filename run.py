aa861991917f9950e8fb1f4b27888a4634efdf88
AlbinHall <albin-hall1@hotmail.com>
2022-10-26 19:31:04 +0000

    add function for user chose color

--- run.py
+++ run.py
@@ -6,19 +6,39 @@ black = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36]
 green = [0]
 
 
-def Choice_of_color():
+ def Choice_of_color():
+    global red
+    global black
+    global green
+    
+    input_color = input("Choose your color: ")
+
+    if value(input_color).lower() is red:
+        player_choice == red
+        print("Red is your choice\n")
+    elif input_color == black:
+        player_choice = black
+        print("Black is your choice\n")
+    elif input_color == green:
+        player_choice = green
+        print("Green is your choice\n")
+    else:
+        print("pick beetween red, black and green")
+        Choice_of_color()
 
 
-def random_number_selector():
+#def random_number_selector():
 
 
-def the_bank():
+#def the_bank():
 
 
-def calculate_the_stake():
+#def calculate_the_stake():
 
 
-def losing_the_bet
+#def losing_the_bet
 
 
-def main()
+#def main()
+
+Choice_of_color()
